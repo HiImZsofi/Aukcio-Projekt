7a42e3e56ae0eb2b773b69d95d6fdf67f2eaacc4
GyZsofi <zsofia.gyori00@gmail.com>
2022-09-22 11:12:28 +0200

    user paintings to list -eddig jutottam az óra végéig-

--- src/hu/petrik/fest/Main.java
+++ src/hu/petrik/fest/Main.java
@@ -1,8 +1,12 @@
 package hu.petrik.fest;
 
+import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.System.in;
 
 public class Main {
     static List<Festmenyek> festmenyek;
@@ -16,6 +20,23 @@ public class Main {
         festmenyek = new ArrayList<>();
         festmenyek.add(new Festmenyek("elso", "elsofesto", "stilus1"));
         festmenyek.add(new Festmenyek("masodik", "masodikfesto", "stilus2"));
+
+        System.out.println("Adjon meg egy számot:");
+        Scanner sc = new Scanner(in);
+        int szam = Integer.parseInt(sc.nextLine());
+        int probalkozas = 0;
+        while(szam > probalkozas){
+            probalkozas++;
+            System.out.println("A kép festője: ");
+            String festo = sc.nextLine();
+            System.out.println("A kép címe: ");
+            String cim = sc.nextLine();
+            System.out.println("A kép stílusa: ");
+            String stilus = sc.nextLine();
+            festmenyek.add(new Festmenyek(festo, cim, stilus));
+            System.out.println("Successfully added");
+        }
+
 
     }
 }
