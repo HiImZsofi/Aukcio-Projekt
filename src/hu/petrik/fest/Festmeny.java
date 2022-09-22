baef48e119e3a8f354823f8173c2feec360bf406
GyZsofi <zsofia.gyori00@gmail.com>
2022-09-22 09:12:46 +0200

    peldanyositas

--- src/hu/petrik/fest/Festmeny.java
+++ src/hu/petrik/fest/Festmeny.java
@@ -1,4 +1,64 @@
 package hu.petrik.fest;
 
+import java.time.LocalDateTime;
+
 public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private LocalDateTime utolsoLicitIdeje;
+    private int magasLicit;
+    private Boolean elkelt;
+
+    public Festmeny(String cim, String festo, String stilus, int licitekSzama, LocalDateTime utolsoLicitIdeje, int magasLicit, Boolean elkelt) {
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+        this.licitekSzama = licitekSzama;
+        this.utolsoLicitIdeje = LocalDateTime.now();
+        this.magasLicit = magasLicit;
+        this.elkelt = elkelt;
+    }
+
+    public String getFesto() {
+        return festo;
+    }
+
+    public String getStilus() {
+        return stilus;
+    }
+
+    public int getLicitekSzama() {
+        return licitekSzama;
+    }
+
+    public LocalDateTime getUtolsoLicitIdeje() {
+        return utolsoLicitIdeje;
+    }
+
+    public int getMagasLicit() {
+        return magasLicit;
+    }
+
+    public Boolean getElkelt() {
+        return elkelt;
+    }
+
+    public void setElkelt(Boolean elkelt) {
+        this.elkelt = elkelt;
+    }
+
+    @Override
+    public String toString() {
+        return "Festmeny{" +
+                "cim='" + cim + '\'' +
+                ", festo='" + festo + '\'' +
+                ", stilus='" + stilus + '\'' +
+                ", licitekSzama=" + licitekSzama +
+                ", utolsoLicitIdeje=" + utolsoLicitIdeje +
+                ", magasLicit=" + magasLicit +
+                ", elkelt=" + elkelt +
+                '}';
+    }
 }
--- src/hu/petrik/fest/Main.java
+++ src/hu/petrik/fest/Main.java
@@ -1,7 +1,12 @@
 package hu.petrik.fest;
 
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Festmeny festmeny1 = new Festmeny("asd", "Van Gogh", "reneszansz", 0, LocalDateTime.now(),
+                0, false);
+
+
     }
 }
